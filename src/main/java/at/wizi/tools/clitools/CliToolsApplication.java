f52b0959f3ce04a1b08910c519c3659fbd4ce45e
wizi <rene.witzelsperger@mic-cust.com>
2023-06-19 15:53:30 +0200

    init spring cli tools

---
+++ src/main/java/at/wizi/tools/clitools/CliToolsApplication.java
@@ -0,0 +1,13 @@
+package at.wizi.tools.clitools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/wizi/tools/clitools/CliToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.wizi.tools.clitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
