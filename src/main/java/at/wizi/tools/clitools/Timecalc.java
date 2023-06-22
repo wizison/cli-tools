cfc92f70f6470d6876032aedf6de8d08d6e4ad4b
wizi <rene.witzelsperger@mic-cust.com>
2023-06-22 16:04:25 +0200

    init time calc class

---
+++ src/main/java/at/wizi/tools/clitools/Timecalc.java
@@ -0,0 +1,34 @@
+package at.wizi.tools.clitools;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "timecalc")
+public class Timecalc implements Callable<Integer> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Timecalc.class);
+
+    @CommandLine.Option(names = {"-s", "--start"}, description = "start time")
+    private String start = "08:00";
+    @CommandLine.Option(names = {"-e", "--end"}, description = "end time", required = true)
+    private String end;
+    @CommandLine.Option(names = {"-p", "--pause"}, description = "pause in minutes")
+    private int pause = 0;
+
+    @Override
+    public Integer call() throws Exception {
+
+        LOG.info("parameters: start: {}, end: {}, pause: {}", start, end, pause);
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Timecalc()).execute(args);
+        System.exit(exitCode);
+    }
+}
